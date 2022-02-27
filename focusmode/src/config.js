ee00db4dfd47f3991069143210aeb2b4b7708f9f
poojakedia <poojakedianov@gmail.com>
2022-02-27 07:02:36 -0500

    rock paper scissors mini game

---
+++ focusmode/src/config.js
@@ -0,0 +1,11 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "3c637e01807e4246b7838f2afc9e76b8";
+export const redirectUri = "http://localhost:3000";
+export const clientSecret = "85efb3dd061e40f79120b64f086b340d";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
