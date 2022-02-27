b87628449618182227accbfe9980d3142f30d3ab
boniyunanda <68647238+boniyunanda@users.noreply.github.com>
2022-02-27 16:50:58 +0700

    focusapp

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import Timer from "./Timer";
+import Settings from "./Settings";
+import Game from "./Game";
+import Music from "./Music";
+import landingScreen from "./landingScreen";
+import {useState} from "react";
+import SettingsContext from "./SettingsContext";
+
+function App() {
+
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(0.5);
+  const [breakMinutes, setBreakMinutes] = useState(0.5);
+
+  return (
+    <main>
+      <div>
+        <h1>FocusMode</h1>
+      </div>
+      <landingScreen />
+      <div className="timer">
+        <SettingsContext.Provider value={{
+          showSettings,
+          setShowSettings,
+          workMinutes,
+          breakMinutes,
+          setWorkMinutes,
+          setBreakMinutes,
+        }}>
+          {/* {showSettings ? <Settings /> : <Timer />} */}
+          {showSettings ? <Settings /> : <Timer />}
+        </SettingsContext.Provider>
+      </div>
+      <img className='dino-img' src={require('./img/dino.gif')}/>
+      {/* <Game/> */}
+      <Music/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Music.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import {Howl} from "howler";
+import MusicBtn from "./MusicBtn";
+import './App.css';
+
+
+const audioClips =[
+{sound: "http://www.musica-ferrum.com/downloads/easybeans/A%20Dream%20of%20Summer.mp3"}
+]
+
+class Music extends Component{
+
+    soundPlay = (src) =>{
+        const sound = new Howl({
+            src,
+            html5: true
+        })
+        sound.play();
+    }
+
+    RenderButtonSound = () =>{
+        return audioClips.map((soundObj, index)=>{
+            return(
+                <MusicBtn key={index} onClick={() => this.soundPlay(soundObj.sound)}/>
+            );
+        })
+    }
+
+    render(){
+    return(
+        <div className="music">
+            {this.RenderButtonSound()}
+            <h1 className="songDec">A Dream Of Summer</h1>
+        </div>
+    );}
+
+
+}
+
+export default Music;
