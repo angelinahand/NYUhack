cb4f6a0bdbd277b7f7fbf5a24700dddee864fae7
judygong15 <judygong@hotmail.com>
2022-02-27 05:53:56 -0800

    play/pause works on music player

--- src/Music.js
+++ src/Music.js
@@ -1,40 +1,62 @@
 import React, { Component } from "react";
 import {Howl} from "howler";
+import ReactHowler from 'react-howler';
 import MusicBtn from "./MusicBtn";
+import PauseButton from "./PauseButton";
 import './App.css';
+import {useState} from "react";
 
 
-const audioClips =[
-{sound: "http://www.musica-ferrum.com/downloads/easybeans/A%20Dream%20of%20Summer.mp3"}
-]
+// const audioClips =[
+// {sound: "http://www.musica-ferrum.com/downloads/easybeans/A%20Dream%20of%20Summer.mp3"}
+// ]
 
-class Music extends Component{
+// class Music extends Component{
 
-    soundPlay = (src) =>{
-        const sound = new Howl({
-            src,
-            html5: true
-        })
-        sound.play();
-    }
+//     soundPlay = (src) =>{
+//         const sound = new Howl({
+//             src,
+//             html5: true
+//         })
+//         sound.play();
+//     }
 
-    RenderButtonSound = () =>{
-        return audioClips.map((soundObj, index)=>{
-            return(
-                <MusicBtn key={index} onClick={() => this.soundPlay(soundObj.sound)}/>
-            );
-        })
-    }
+//     RenderButtonSound = () =>{
+//         return audioClips.map((soundObj, index)=>{
+//             return(
+//                 <MusicBtn key={index} onClick={() => this.soundPlay(soundObj.sound)}/>
+//             );
+//         })
+//     }
 
-    render(){
-    return(
+//     render(){
+//     return(
+//         <div className="music">
+//             {this.RenderButtonSound()}
+//             <h2 className="songDec">A Dream Of Summer</h2>
+//         </div>
+//     );}
+
+
+// }
+
+// export default Music;
+
+function Music() {
+
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    return (
         <div className="music">
-            {this.RenderButtonSound()}
-            <h2 className="songDec">A Dream Of Summer</h2>
+            <ReactHowler 
+                src="http://www.musica-ferrum.com/downloads/easybeans/A%20Dream%20of%20Summer.mp3"
+                loop={true}
+                playing={isPlaying}
+            />
+            {isPlaying ? <PauseButton className="musicPause" onClick={() => setIsPlaying(!isPlaying)}/> : <MusicBtn onClick={() => setIsPlaying(!isPlaying)}/>}
+           <h2 className="songDec">A Dream Of Summer</h2>
         </div>
-    );}
-
-
+    )
 }
 
-export default Music;
+export default Music
